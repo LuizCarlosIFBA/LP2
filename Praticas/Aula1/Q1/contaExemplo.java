f920a32e86730e6e93ae9ba2caabd4e84aee9ec0
Luiz Carlos <sacramento.tecnico@hotmail.com>
2022-04-17 12:23:18 -0300

    Create contaExemplo.java

---
+++ Praticas/Aula1/Q1/contaExemplo.java
@@ -0,0 +1,60 @@
+
+/**
+ * Escreva uma descrição da classe Conta aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Conta
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private String cpf, nomeCorrentista;
+    private float saldo;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNomeCorrentista() {
+        return nomeCorrentista;
+    }
+
+    public void setNomeCorrentista(String nomeCorrentista) {
+        this.nomeCorrentista = nomeCorrentista;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    /**
+     * Construtor para objetos da classe Conta
+     */
+    public void depositar(float dinheiro)
+    {
+        this.saldo = saldo + dinheiro;   
+    }
+    
+    public float sacar(float dinheiro)
+    {
+        if(getSaldo()>=0){
+            this.saldo = saldo - dinheiro;   
+        }
+        return dinheiro;
+    }
+    
+    public void consultar(){
+        System.out.println("CPF: "+getCpf());
+        System.out.println("Nome do correntista:"+nomeCorrentista);
+        System.out.println("Saldo: "+getSaldo());
+    }
+
+}
